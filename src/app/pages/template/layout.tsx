2897ecd20cec9c5d27b9a67022c26db54185a988
Elias Sebastian Espinoza <eliassebastian.espinoza@axa-assistance.es>
2022-08-23 09:38:11 +0200

    Fix imports layout

---
+++ src/app/pages/template/layout.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import StickyFooter from "../footer/footer";
+import Header from "../header/header";
+import { LayoutContainer, MainContainer } from "../layout/layout-styles";
+
+const Layout = ({ children }: any) => {
+  return (
+    <LayoutContainer>
+      <Header />
+      <MainContainer>{children}</MainContainer>
+      <StickyFooter />
+    </LayoutContainer>
+  );
+};
+
+export default Layout;
